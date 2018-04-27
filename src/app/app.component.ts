fd6e015a0b9eaf8c12afdd0f052afaaddcd2dec2
Matthew L. Gross <matt.gross@wwinc.com>
2018-04-27 12:35:03 -0500

    Added teams names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { EspnService } from './espn.service';
+import { TodayGameList } from './today-game-list';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,13 @@ export class AppComponent implements OnInit {
   leagues: String[];
   form;
   gameData;
+  isPopulated = false;
+  public todayGameList: Array<TodayGameList>;
+  
 
-  constructor(private espnService: EspnService){}
+  constructor(private espnService: EspnService){
+    this.todayGameList = new Array<TodayGameList>();
+  }
 
   ngOnInit() {
     this.leagues = ['MLB', 'NHL', 'NBA'];
@@ -24,7 +31,33 @@ export class AppComponent implements OnInit {
     })
   }
 
-  getMlb(): void{this.espnService.getMlb().subscribe(data => this.gameData = JSON.stringify(data))};
+  populateTodayGameList(data) {
+    // First loop through each game for game data
+    for(let game of data.games) {
+      let newGame = new TodayGameList;
+      newGame.away_team_score = game.away_team_score;
+      newGame.home_team_score = game.home_team_score;
+      newGame.period = game.period;
+      newGame.period_label = game.period_label;
+      newGame.started_at = game.started_at;
+      // Get team names
+      for (let away_team of data.away_teams){
+        if (away_team.id == game.away_team_id) {
+          newGame.away_team = away_team.nickname;
+        }
+      }
+      for (let home_team of data.home_teams){
+        if (home_team.id == game.home_team_id) {
+          newGame.home_team = home_team.nickname;
+        }
+      }
+      // Send it to the array
+      this.todayGameList.push(newGame);
+    }
+    this.isPopulated = true;
+  }
+
+  getMlb(): void{this.espnService.getMlb().subscribe(data => this.populateTodayGameList(data))};
 
   onSubmit(formValues) {
     console.log(formValues);
---
+++ src/app/today-game-list.ts
@@ -0,0 +1,9 @@
+export class TodayGameList{
+    away_team: string;
+    home_team: string;
+    away_team_score: string;
+    home_team_score: string;
+    period: string;
+    period_label: string;
+    started_at: string;
+}
